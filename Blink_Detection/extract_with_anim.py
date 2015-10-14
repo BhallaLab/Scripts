404f6189ed68411e3940b5c28448c6e274d63df6
Dilawar Singh <dilawars@ncbs.res.in>
2015-10-14 22:02:54 +0530

    This version uses imshow to display the figure in inset. IT makes it slow.

--- Blink_Detection/extract_with_anim.py
+++ Blink_Detection/extract_with_anim.py
@@ -18,14 +18,8 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 import extract
 import sys
-from collections import OrderedDict
 import cv2
 import webcam
-
-window_size_ = 7
-# The mean area multiplied by this factor. Anything below this belongs to blink
-# zone.
-threshold_factor_area_ = 0.5
 
 ######################################
 # Initialize animation here 
@@ -43,6 +37,9 @@ ax2 = ax1.twinx()
 ax3 = fig_.add_subplot(2, 1, 2)
 ax4 = ax3.twinx()
 
+# Inset for raw data.
+fig_ax_ = fig_.add_axes([.7, .55, .2, .2], axisbg='y')
+
 axes_ = { 'raw' : ax1, 'raw_twin' : ax2, 'blink' : ax3, 'blink_twin' : ax4 }
 lines_["rawA"] = ax1.plot([], [], color='blue')[0]
 lines_["rawB"] = ax2.plot([], [], color='red')[0]
@@ -51,14 +48,13 @@ lines_['blinkB'] = ax4.plot([], [], 'p', color = 'red')[0]
 
 time_template_ = 'Time = %.1f s'
 time_text_ = fig_.text(0.05, 0.9, '', transform=axes_['blink'].transAxes)
-cv2.namedWindow('image')
 
 tvec_ = []
 y1_ = []
 y2_ = []
 
 def init():
-    global data_, axes_, lines_
+    global axes_, lines_
     global box_, fps_
     global cap_
     videoFile = sys.argv[1]
@@ -66,10 +62,8 @@ def init():
     fps_ = cap_.get(cv2.cv.CV_CAP_PROP_FPS)
     ret, fstFrame = cap_.read()
     box_ = webcam.get_bounding_box(fstFrame)
-    try:
-        cv2.destroyWindows('Bound_eye')
-    except Exception as e:
-        pass
+    cv2.waitKey(1)
+    cv2.destroyAllWindows()
     return lines_.values()
 
 def update_axis_limits(ax, x, y):
@@ -87,15 +81,15 @@ def animate(i):
     global box_
     global tvec_, y1_, y2_
     global cap_
+    global fig_ax_
 
     t = float(i) / fps_
     ret, img = cap_.read()
     (x0, y0), (x1, y1) = box_
     frame = img[y0:y1,x0:x1]
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    # display the whole frame in opencv2. Very cool.
-    cv2.imshow("image", frame)
-    cv2.waitKey(1)
+
+    fig_ax_.imshow(frame[::2,::2], interpolation='nearest', animated=True)
 
     inI, outI, edge, pixal = webcam.process_frame(gray)
     tvec_.append(t); y1_.append(edge); y2_.append(pixal)
@@ -117,18 +111,31 @@ def animate(i):
     time_text_.set_text(time_template_ % t)
     return lines_.values(), time_text_
 
-ani_ = anim.FuncAnimation(fig_
+def get_blinks( csvFile ):
+    global ani_, cap_
+    ani_ = anim.FuncAnimation(fig_
         , animate
         , init_func=init
         , blit = False
-        , interval = 1
+        , interval = 10
         , repeat = False
         )
 
-plt.show()
-cap_.release()
-cv2.destroyAllWindows()
+    plt.show( )
+    cap_.release()
 
-outfile = '%s_out.csv' % sys.argv[1]
-print("[INFO] Writing to file %s" % outfile)
-np.savetxt(outfile, data_, delimiter=',' ,header="time,edge,pixal")
+def main():
+    csvFile = sys.argv[1]
+    try:
+        get_blinks(csvFile)
+    except:
+        outfile = '%s_out.csv' % sys.argv[1]
+        print("[INFO] Writing to file %s" % outfile)
+        np.savetxt(outfile, data_, delimiter=',' ,header="time,edge,pixal")
+
+    outfile = '%s_out.csv' % sys.argv[1]
+    print("[INFO] Writing to file %s" % outfile)
+    np.savetxt(outfile, data_, delimiter=',' ,header="time,edge,pixal")
+
+if __name__ == '__main__':
+    main()
