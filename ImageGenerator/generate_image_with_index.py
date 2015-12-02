642c261fa23bd3a543a521a76454fdf4d6ce3f31
Dilawar Singh <dilawars@ncbs.res.in>
2015-12-02 14:58:43 +0530

    Added description on script on the script.

--- ImageGenerator/generate_image_with_index.py
+++ ImageGenerator/generate_image_with_index.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+"""generate_image_with_index.py: 
+
+Generates transparent image with numbers written on them. Can be used to
+caliberate projectors for frame per seconds.
+
+"""
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2015, Dilawar Singh and NCBS Bangalore"
+__credits__          = ["NCBS Bangalore"]
+__license__          = "GNU GPL"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+__status__           = "Development"
+
+
 import matplotlib.pyplot as plt
 import os
 import sys
@@ -17,6 +34,6 @@ for i in range(1000):
     fig = plt.figure( figsize=(width, height) )
     plt.axis('off')
     plt.plot()
-    plt.figtext(0.4, 0.25, '1', fontsize=fontsize)
+    plt.figtext(0.4, 0.4, '%s' % i, fontsize=fontsize)
     plt.savefig('_images/%s.png' % i, dpi=dpi, transparent=True)
 print("[INFO] All images are saved to _images")
