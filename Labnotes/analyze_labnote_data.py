c7631fa084a68e310219c68ba7ff760175ee57e6
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-08-05 22:15:56 +0530

    Overall analysis.

---
+++ Labnotes/analyze_labnote_data.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2019-, Dilawar Singh"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+
+import sys
+import os
+import matplotlib as mpl
+# mpl.use( 'pgf' )
+import matplotlib.pyplot as plt
+mpl.style.use( ['bmh', 'fivethirtyeight'] )
+import numpy as np
+import pickle
+import scipy.stats as stats
+from collections import Counter
+from datetime import datetime
+
+def parse_date(date):
+    date = date.split('to')[0].strip()
+    return datetime.strptime(date, '%Y-%m-%d')
+
+def reformat(xs):
+    return [parse_date(xs[0])] + xs[1:]
+
+def plot(data):
+
+    plt.figure(figsize=(10, 6))
+    gridSize = (2, 2)
+    ax1 = plt.subplot2grid( gridSize, (0,0), colspan = 1 )
+    ax2 = plt.subplot2grid( gridSize, (0,1), colspan = 1 )
+    ax3 = plt.subplot2grid( gridSize, (1,0), colspan = 1 )
+    ax4 = plt.subplot2grid( gridSize, (1,1), colspan = 1 )
+
+    # distribution by day.
+    days = [int(x[0].strftime('%w')) for x in data]
+    hist = Counter(days)
+    xs = range(7)
+    ys = [hist[x] for x in xs]
+    N = sum(ys)
+    ysn = [hist[x]/N for x in xs]
+    ax11 = ax1.twinx()
+    ax11.plot(xs, ysn, '-o', color='red')
+    ax11.set_ylim(bottom=0)
+    ax11.set_ylabel('frac', fontsize='small')
+    ax1.set_title('By Day')
+    ax1.bar(xs, ys)
+    ax1.set_xticks(xs)
+    ax1.set_xticklabels(['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+            , fontsize=10
+            )
+
+    # distribution by month.
+    temps = [20.8,22.9,25.5,27.1,26.9,24.4,23.3,23.4,23.2,23.1,21.8,20.7]
+    months = [int(x[0].strftime('%m')) for x in data]
+    hist = Counter(months)
+    xs = range(1, 13)
+    ys = [hist.get(x, 0) for x in xs]
+    ax2.bar(xs, ys)
+    ax2.set_xticks(xs)
+    ax2.set_title('By Month')
+    ax22 = ax2.twinx()
+    ax22.plot(xs, temps, color='red')
+    ax22.set_ylim(bottom=17)
+    ax22.set_ylabel('Avg Temp of Bangalore')
+
+    # correlation between temp and labnotes.
+    slope, intercept, r_value, p_value, std_err = stats.linregress(temps, ys)
+    line = slope*np.array(temps)+intercept
+    ax3.scatter(temps, ys)
+    ax3.plot(temps, line) 
+    ax3.set_title(f'p={p_value:.2g}, R-sqr={r_value**2:.2f}')
+    ax3.set_xlabel('Bangalore Avg Temp')
+    ax3.set_ylabel('#Labnotes')
+
+    # Average rate of users.
+    users = set([x[-1] for x in data])
+    boxes = []
+    print( f"[INFO ] Total users {len(users)}" )
+    X = []
+    for i, u in enumerate(users):
+        v = [x[0] for x in data if x[-1] == u]
+        v = sorted(v)
+        # if the latest labenote is less than a month old, ignore.
+        if (datetime.now() - v[-1]).days > 60:
+            print( f"[INFO ] User {u} is ignored." )
+            continue
+        X.append(u)
+        vdiff = [x.days for x in np.diff(v)]
+        boxes.append(vdiff)
+    ax4.boxplot(boxes, whis='range')
+    ax4.set_xticklabels(X, rotation=90)
+
+
+    plt.tight_layout()
+    plt.savefig( f'results.png')
+
+    
+
+def main():
+    with open('data.pickle', 'rb') as f:
+        data = pickle.load(f)
+    data = [ reformat(x) for x in data]
+    plot(data)
+
+if __name__ == '__main__':
+    main()
---
+++ Labnotes/fetch_data.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+import re
+import mechanicalsoup as ms
+import pickle
+
+class Args: pass 
+args = Args()
+
+def page_url(baseUrl, page):
+    return baseUrl + f'?page={page}'
+
+def process(u, br):
+    global args
+    print( f"[INFO ] Analysing {u.text}" )
+    url = args.url  + u.attrs['href']
+
+    # First go to this url.
+    r = br.open(url)
+    if not r.ok:
+        print( f"[WARN ] Could not open labnote for {url}" )
+    
+    # Find the last page for this user.
+    html = br.get_current_page()
+    lastPage = html.find('a', title='Go to last page')
+    if not lastPage:
+        lastPage = 1
+    else:
+        m = re.search(r'page\=(\d+)', lastPage.attrs['href'])
+        lastPage = int(m.group(1))
+
+    pageUrls = [ page_url(url, i) for i in range(1,1+lastPage)]
+    for url in pageUrls:
+        br.open(url)
+        page = br.get_current_page()
+        print(page)
+        break
+    
+def process_page(baseurl, i, br):
+    url = baseurl + f'&page={i}'
+    # open the url and apge
+    print( f"[INFO ] Fetching page {i}", url )
+    r = br.open(url)
+    assert r.ok
+    html = br.get_current_page()
+    trs = html.find_all('tr', recursive=True)
+    res = []
+    for tr in trs:
+        tds = tr.find_all('td')
+        tds = [x.text.strip() for x in tds]
+        if len(tds) == 3:
+            res.append(tds)
+    print(res)
+    return res
+
+
+def main():
+    global args
+    browser = ms.StatefulBrowser()
+    browser.open(args.url)
+    f = browser.select_form()
+    browser['name'] = args.user
+    browser['pass'] = args.password
+    res = browser.submit_selected()
+    if not res.ok:
+        print( 'Failed to login.')
+        quit(1)
+
+    url = 'https://labnotes.ncbs.res.in/bhalla-lab/experiment-dates?title=&name=&date_filter%5Bmin%5D%5Byear%5D=2016&date_filter%5Bmin%5D%5Bmonth%5D=1&date_filter%5Bmin%5D%5Bday%5D=1&date_filter%5Bmax%5D%5Byear%5D=2022&date_filter%5Bmax%5D%5Bmonth%5D=1&date_filter%5Bmax%5D%5Bday%5D=1&field_bhalla_keywords_tid='
+    h = browser.open(url)
+    assert h.ok
+    html = browser.get_current_page()
+    # find the last page index.
+    lastPage = html.find('a', title='Go to last page')
+    if not lastPage:
+        lastPage = 1
+    else:
+        m = re.search(r'page\=(\d+)', lastPage.attrs['href'])
+        lastPage = int(m.group(1))
+    print( f"[INFO ] Last page is {lastPage}" )
+
+    data = []
+    for i in range(1, lastPage+1):
+        data += process_page(url, i, browser)
+    with open('data.pickle', 'wb') as f:
+        pickle.dump(data, f)
+    print('all done')
+    
+    
+
+if __name__ == '__main__':
+    import argparse
+    # Argument parser.
+    description = '''description'''
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--url', '-U'
+            , required = False, default = 'https://labnotes.ncbs.res.in'
+            , help = 'labnote URL'
+            )
+    parser.add_argument('--user', '-u'
+            , required = True
+            , help = 'User name'
+            )
+    parser.add_argument( '--password', '-p'
+            , required = True
+            , type = str
+            , help = 'password'
+            )
+    parser.parse_args(namespace=args)
+    main()
